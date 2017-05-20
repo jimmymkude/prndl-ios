9f48b1b6625a1d89f2ed3b46f8d082171a1eb64f
Jimmy Mkude <noone@jimmys-mac.local>
2017-05-20 07:36:00 -0700

    make repo

---
+++ prndl/ParkerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ParkerViewController.swift
+//  prndl
+//
+//  Created by Jimmy Mkude on 5/19/17.
+//  Copyright © 2017 Jimmy Mkude. All rights reserved.
+//
+
+import UIKit
+
+class ParkerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ prndl/SignUpViewController.swift
@@ -0,0 +1,56 @@
+//
+//  SignUpViewController.swift
+//  prndl
+//
+//  Created by Jimmy Mkude on 5/19/17.
+//  Copyright © 2017 Jimmy Mkude. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    let menuItems = [
+            "Reservations",
+            "Vehicle Info",
+            "Payment Info",
+            "Upcoming Events",
+            "Invite a Friend"
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.tableFooterView = UIView()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell"/*Identifier*/, for: indexPath as IndexPath)
+        cell.textLabel?.text = menuItems[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return menuItems.count
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
