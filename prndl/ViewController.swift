0f7e5616efb7daa5d89bf8a807bf0a4e6c2b763b
Jimmy Mkude <noone@jimmys-mac.local>
2017-05-19 20:39:30 -0700

    Initial Commit

---
+++ prndl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  prndl
+//
+//  Created by Jimmy Mkude on 5/19/17.
+//  Copyright © 2017 Jimmy Mkude. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
